5f2b600783f056c749c9680f771469ed1db9a660
Alessio <lb.alessio@hotmail.it>
2017-02-08 10:50:25 +0100

    Loading ended
    Ended loading page

---
+++ modules/loading/loading.js
@@ -0,0 +1,36 @@
+function pageLoading(){
+  firstAppear();
+  secondAppear();
+  fadeOutLoadingFrame();
+}
+
+function firstAppear(){
+  firstAppear = document.getElementById('firstAppear');
+  setTimeout(function(){
+    firstAppear.style.WebkitAnimation = 'toOpacity 2s';
+    firstAppear.style.animation = 'toOpacity 2s';
+    firstAppear.style.opacity = '1';
+  }, 1000);
+}
+
+function secondAppear(){
+  secondAppear = document.getElementById('secondAppear');
+  setTimeout(function(){
+    secondAppear.style.WebkitAnimation = 'toOpacity 2s';
+    secondAppear.style.animation = 'toOpacity 2s';
+    secondAppear.style.opacity = '1';
+  }, 3000);
+}
+
+function fadeOutLoadingFrame(){
+  loadingFrame = document.getElementById('loadingFrame');
+  setTimeout(function(){
+    loadingFrame.style.WebkitAnimation = 'fromOpacity 2s';
+    loadingFrame.style.animation = 'fromOpacity 2s';
+    secondAppear.style.opacity = '0';
+    setTimeout(function(){
+      loadingFrame.style.display ='none';
+    }, 2000)
+  }, 5000);
+
+}
