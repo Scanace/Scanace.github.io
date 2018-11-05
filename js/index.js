d6eebd84833fc6ee835324643e18ca5e8392b09f
Alessio Libardi <alessiolibardi@gmail.com>
2018-11-05 21:07:36 +0100

    Cleaned master repo

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$(window).on('load', function(e){
-  $(this).on('click', function(e){
-    alert("This is just a design sketch. While waiting for the final website release, contact me at 'alessiolibardi@gmail.com'");
-  });
-});
-
-$(document).ready(function(){
-
-});
